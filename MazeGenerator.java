960fbc258d38acc5a184410619c892c7439dc110
Jeff McGlynn <jeff@jeffmcglynn.com>
2010-11-12 02:25:48 -0800

    Added profiling times to generator and solver.

--- MazeGenerator.java
+++ MazeGenerator.java
@@ -5,7 +5,12 @@ public class MazeGenerator {
 	}
 
 	public Maze generateMaze(int width, int height) {
-		DummyGenerator gen = new DummyGenerator();
-		return gen.generate(width, height);
+		long start = System.nanoTime();
+		IGenerator gen = new DummyGenerator();
+		Maze ret = gen.generate(width, height);
+		long end = System.nanoTime();
+		
+		System.out.println("Took " + (double) (end - start) / 1000000000.0 + " seconds to compute maze.");
+		return ret;
 	}
 }
--- MazeSolver.java
+++ MazeSolver.java
@@ -7,7 +7,12 @@ public class MazeSolver
 
     public LinkedList<Pair> solveMaze(Maze maze)
     {
-    	DummySolver ds = new DummySolver();
-        return ds.solveMaze(maze);
+		long start = System.nanoTime();
+    	ISolver ds = new DummySolver();
+    	LinkedList<Pair> ret = ds.solveMaze(maze);
+		long end = System.nanoTime();
+		
+		System.out.println("Took " + (double) (end - start) / 1000000000.0 + " seconds to solve maze.");
+		return ret;
     }
 }
